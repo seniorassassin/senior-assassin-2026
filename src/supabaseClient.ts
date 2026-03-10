905ac104bd5a13b68b402dad6d250fd14dafe78b
seniorassassin <makneekb21@gmail.com>
2026-03-10 11:47:13 +1100

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,7 +1,7 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+// @ts-nocheck
+import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://qgwvtpuvinvjrmipixcv.supabase.co';
-const SUPABASE_KEY =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFnd3Z0cHV2aW52anJtaXBpeGN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwOTMwNzIsImV4cCI6MjA4ODY2OTA3Mn0.DW8pjkC4ysQP8tL6P1shbziP77F5VJ-JWiNDZVtfx9M';
+const SUPABASE_URL = 'https://qgwvtpuvinvjrmipixcv.supabase.co'
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFnd3Z0cHV2aW52anJtaXBpeGN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwOTMwNzIsImV4cCI6MjA4ODY2OTA3Mn0.DW8pjkC4ysQP8tL6P1shbziP77F5VJ-JWiNDZVtfx9M'
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
